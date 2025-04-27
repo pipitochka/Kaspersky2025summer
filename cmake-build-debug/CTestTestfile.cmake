e82dd829bd1309cfc002e6ed3030759828bcda39
pipitochka <aaakulov_3@edu.hse.ru>
2025-04-27 20:59:11 +0300

    delete dirs

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/artemakulov/Study/2 course/2 sem/Kaspersky/mangen
-# Build directory: /Users/artemakulov/Study/2 course/2 sem/Kaspersky/mangen/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[tests]=] "/Users/artemakulov/Study/2 course/2 sem/Kaspersky/mangen/cmake-build-debug/tests")
-set_tests_properties([=[tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/artemakulov/Study/2 course/2 sem/Kaspersky/mangen/CMakeLists.txt;18;add_test;/Users/artemakulov/Study/2 course/2 sem/Kaspersky/mangen/CMakeLists.txt;0;")
